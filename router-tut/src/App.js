509f8bcef06d89a174a71a2a0f5867d622b91df4
siamso <hello.siamahamed@gmail.com>
2024-06-26 14:13:29 +0600

    initializing

--- router-tut/src/App.js
+++ router-tut/src/App.js
@@ -1,7 +1,33 @@
+import Header from './Header.js';
+import Nav from './Nav.js';
+import Home from './Home.js';
+import Footer from './Footer.js';
+import NewPost from './NewPost.js';
+import PostPage from './PostPage.js';
+import About from './About.js';
+import Missing from './Missing.js';
+import { Route, Switch, useHistory} from 'react-router-dom';
+import { useState, useEffect } from 'react'; 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Nav/>
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route exact path='/post'>
+          <NewPost/>
+        </Route>
+        <Route  path='/post/:id'>
+          <PostPage/>
+        </Route>
+        <Route path='/about' component={About} />
+        <Route path='*' component={Missing} />
+      </Switch>
+      <Footer/>
     </div>
   );
 }
